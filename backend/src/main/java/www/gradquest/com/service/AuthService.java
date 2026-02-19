15b4d1b6edb3bae13e2a7cf0adc4d4c0d6f57c2c
lij756664-source <lij756664@gmail.com>
2026-02-19 19:48:49 +0800

    Revert "Merge branch 'main' of https://github.com/Camellia0506-B/Service-Outsourcing-Innovation-and-Entrepreneurship-Competition"
    This reverts commit 3d8449239102c468d7b8abc11f06a905747bb3e6, reversing
    changes made to d2d0a6ac0e4d974f7c9a5b323d8237fbdb878ea1.

--- backend/src/main/java/www/gradquest/com/service/AuthService.java
+++ backend/src/main/java/www/gradquest/com/service/AuthService.java
@@ -1,7 +1,6 @@
 package www.gradquest.com.service;
 
 import org.springframework.web.multipart.MultipartFile;
-import www.gradquest.com.dto.ForgotPasswordSendCodeResponse;
 import www.gradquest.com.dto.LoginResponse;
 import www.gradquest.com.dto.RegisterResponse;
 
@@ -15,14 +14,4 @@ public interface AuthService {
     LoginResponse login(String username, String password);
 
     void logout(Long userId);
-
-    /**
-     * 1.4.1 忘记密码：发送验证码
-     */
-    ForgotPasswordSendCodeResponse forgotPasswordSendCode(String username, String email);
-
-    /**
-     * 1.4.2 忘记密码：校验验证码并重置密码
-     */
-    void forgotPasswordReset(String username, String code, String newPassword);
 }
